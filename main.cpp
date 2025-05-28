f4c2f6fb9593490ef94d76ec50e6663da2b827a0
gusmezomo <gusmezomo@gmail.com>
2025-05-27 21:12:03 -0300

    3

--- main.cpp
+++ main.cpp
@@ -13,23 +13,27 @@ Grafo G1;
 int main() {
 
   string Nome;
+
   Nome = "GrafoADJ.txt";
 
+  cout << "Leitura da Matriz de Adjacencia:" << endl;
   G1.LeMatrizADJ(Nome);
   G1.ImprimeMatrizAdjacencias();
 
   // Conversão da Matriz de Adjacência para Estrutura Encadeada
+  cout << "\nConversao para Matriz de Incidencia:" << endl;
   G1.ConverteDaMatrizDeAdjacenciasParaEstruturaEncadeada();
-
   G1.SalvaMatrizINC(Nome);
 
   // Ou 
 
+  Nome = "GrafoINC.txt";
+  
+  cout << "\nLeitura da Matriz de Incidencia:" << endl;
   G1.LeMatrizINC(Nome);
 
   // Conversão da Matriz de Incidência para Estrutura Encadeada
+  cout << "\nConversao para Matriz de Adjacencia:" << endl;
   G1.ConverteDaMatrizDeIncidenciasParaEstruturaEncadeada();
-
   G1.SalvaMatrizADJ(Nome);
-  
 }
